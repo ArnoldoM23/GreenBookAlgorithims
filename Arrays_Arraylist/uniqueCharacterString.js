1665c5f39fe9234374d20938d7a2ddf174d7c8da
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-09-11 18:53:09 -0700

    first commit

---
+++ Arrays_Arraylist/uniqueCharacterString.js
@@ -0,0 +1,18 @@
+var unique = function(string){
+	var alphabet = ['a','b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	if (string.lengh > alphabet.lengh) {
+		return false;
+	}
+
+}
+
+// split each string to check if it is unique.
+
+// check characters with the alphabet to make sure that it is unique
+
+// create and array with the alphabet.
+
+// check the lengh of string
+ // if greater than 
+
+// loop over the string use i as the flag 
