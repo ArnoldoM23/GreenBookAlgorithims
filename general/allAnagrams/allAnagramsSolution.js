b4a7728228f412b2c76e7a666a58c64585822081
AnorldoM23 <arnoldomunoz23@yahoo.com>
2017-01-10 16:00:06 -0800

    linklistCycle solution

---
+++ general/allAnagrams/allAnagramsSolution.js
@@ -0,0 +1,76 @@
+/**
+ * Given a single input string, write a function that produces all possible anagrams
+ * of a string and outputs them as an array. At first, don't worry about
+ * repeated strings.  What time complexity is your solution?
+ *
+ * Extra credit: Deduplicate your return array without using uniq().
+ */
+
+/**
+  * example usage:
+  * var anagrams = allAnagrams('abc');
+  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+  */
+
+var allAnagrams = function(string) {
+  // Your code here.
+// create an object to store the each anagram. The onject will also prevent from having duplicates
+  var uniqueOutput = {};
+// create a subrutine that will recurse. It should take an empty string will use to add the letters of each anagram. The second parameter should be the string that we are making anagrams of.
+  (function anagram (ana, str) {
+    // base case. If the string being passed in is empty create a new property in our storage object with the anagram as the key and give it a value of 1 or true.
+    if (str === '') { uniqueOutput[ana] = 1; }
+// loop over the length of the string that is being passed in.
+    for (var i = 0; i < str.length; i++) {
+    // invoke the recursive function passing in the empty and concatate it with the current letter. The second parameter will be the string being sliced starting at zero and slicing the number of i. plus concatate it with the another slice of the string starting at i plus one.
+      anagram(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+    }
+    // invoke the recursive function passing in an empty string as the anagram and the string being checked as the second parameter.
+  })('', string);
+// once completed return the the object keys as an array this can be done by using Object.keys method.
+  return Object.keys(uniqueOutput);
+};
+
+
+
+
+function getAllAnagrams(string){
+  var allAnagrams = {};
+  function getAnagram(anagram, string){
+    if (string === '') { allAnagrams[anagram] = 1;}
+    for (var i = 0; i < string.length; i++) {
+      // for the first iteration
+      // b , abc
+      // call recurse
+       // for the first iteration of the first iteration
+      // a+b = ab, c
+      // call recurse
+       // for the first iteration of the first iteration  iteration
+       // ab+c = abc, ''
+      // for the second iteration of the first iteration
+      // a+c =ac, b
+      // ac+b =acb , ''
+      getAnagram(anagram + string[i], string.slice(0, i) + string.slice(i + 1))
+    }
+  };
+  getAnagram('', string);
+  return Object.keys(allAnagrams);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
