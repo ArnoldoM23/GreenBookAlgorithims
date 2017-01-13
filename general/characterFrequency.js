02b1e2d622ad43143770f228ca55dd7b6bf5ce89
AnorldoM23 <arnoldomunoz23@yahoo.com>
2017-01-13 15:03:15 -0800

    binary heap

---
+++ general/characterFrequency.js
@@ -0,0 +1,54 @@
+/*
+ *  Write a function that takes as its input a string and returns an array of
+ *  arrays as shown below sorted in descending order by frequency and then by
+ *  ascending order by character.
+ *
+ *       :: Example ::
+ *
+ *  characterFrequency('mississippi') ===
+ *  [
+ *    ['i', 4],
+ *    ['s', 4],
+ *    ['p', 2],
+ *    ['m', 1]
+ *  ]
+ *
+ *       :: Example2 ::
+ *
+ *  characterFrequency('miaaiaaippi') ===
+ *  [
+ *    ['a', 4],
+ *    ['i', 4],
+ *    ['p', 2],
+ *    ['m', 1]
+ *  ]
+ *
+ *       :: Example3 ::
+ *
+ *  characterFrequency('mmmaaaiiibbb') ===
+ *  [
+ *    ['a', 3],
+ *    ['b', 3],
+ *    ['i', 3],
+ *    ['m', 3]
+ *  ]
+ *
+ */
+
+
+var characterFrequency = function(string) {
+	const characters = {};
+	for (var i = 0; i < string.length; i++) {
+		if(!characters[string[i]]) {
+			characters[string[i]] = 1;
+		}else{
+			characters[string[i]]++;
+		}
+	}
+	return characters
+  // return result;
+};
+
+
+console.log(characterFrequency('aabbc'))
+
